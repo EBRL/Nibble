ca7f3a309976347997e3688afb80fadd39f6dc47
Scott Burns <scott.s.burns@gmail.com>
2011-07-27 17:05:21 -0500

    initial configurator

---
+++ nibble/config.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+"""
+Copyright (c) 2011, Scott Burns
+All rights reserved.
+"""
+
+try:
+    import yaml
+except ImportError:
+    print "You must install PyYAML. Try $ easy_install pyyaml"
+    raise
+
+
+
+if '__name__' == '__main__':
+    """BEGIN HERE"""
