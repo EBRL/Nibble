466e6a3e62334db48ae80032071551acf4244aee
Scott Burns <scott.s.burns@gmail.com>
2011-07-29 16:10:13 -0500

    another  file move

---
+++ nibble/nibconfig.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+"""
+Copyright (c) 2011, Scott Burns
+All rights reserved.
+"""
+import os 
+
+try:
+    import yaml
+except ImportError:
+    print "You must install PyYAML. Try $ easy_install pyyaml"
+    raise
+
+
+class Configurator(object):
+    """ Main class for combining multiple yaml files to one data structure
+    """
+    def __init__(self, yaml_file_list):
+        """ Constructor
+    
+        Parameters
+        ----------
+        yaml_file_list: seq
+            all yaml files that should be coerced into one data structure
+        """
+        exist = map(os.path.isfile, yaml_file_list)    
+        if not all(exist):
+            raise IOError('Not all files specified in the constructor exist')
+        self.yaml_files = yaml_file_list
+    
