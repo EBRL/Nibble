4351e47e35f5676f8bfef56d69351c7aae4773af
Scott Burns <scott.s.burns@gmail.com>
2011-07-29 16:10:23 -0500

    little test file

---
+++ bin/nibble.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+"""
+Copyright (c) 2011, Scott Burns
+All rights reserved.
+"""
+
+    
+
+
+if __name__ == '__main__':
+    from nibconfig import Configurator
+    cfg = Configurator(['/Users/scottburns/Documents/code/nibble/configurations/cutting_global.yaml',
+                        '/Users/scottburns/Documents/code/nibble/configurations/LDRC_KKI.yaml'])
