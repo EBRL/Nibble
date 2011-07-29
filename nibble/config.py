c8bd2f68db9dfc14105e864c65cd631a0fc8a972
Scott Burns <scott.s.burns@gmail.com>
2011-07-29 15:51:02 -0500

    beginning Configurator object

--- nibble/config.py
+++ nibble/config.py
@@ -4,6 +4,7 @@
 Copyright (c) 2011, Scott Burns
 All rights reserved.
 """
+import os 
 
 try:
     import yaml
@@ -12,6 +13,20 @@ except ImportError:
     raise
 
 
-
-if '__name__' == '__main__':
-    """BEGIN HERE"""
+class Configurator(object):
+    """ Main class for combining multiple yaml files to one data structure
+    """
+    def __init__(self, yaml_file_list):
+        """ Constructor
+    
+        Parameters
+        ----------
+        yaml_file_list: seq
+            all yaml files that should be coerced into one data structure
+        """
+        exist = map(os.path.isfile, yaml_file_list)    
+        if not all(exist):
+            raise IOError('Not all files specified in the constructor exist')
+        yaml_files = yaml_file_list
+    
+    
