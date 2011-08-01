035a5e61805a36b194b05af5459cde822163ffe3
Scott Burns <scott.s.burns@gmail.com>
2011-08-01 10:14:18 -0500

    new test_config function

--- bin/nibble.py
+++ bin/nibble.py
@@ -5,12 +5,14 @@ Copyright (c) 2011, Scott Burns
 All rights reserved.
 """
 
-    
+def test_config():
+    from nibble import config
+    reload(config)
+    cfg = config.Configurator(['/Users/scottburns/Documents/code/nibble/configurations/cutting_global.yaml',
+                        '/Users/scottburns/Documents/code/nibble/configurations/LDRC_KKI.yaml'])
+    all_data = cfg.load_yaml()
+
 
 
 if __name__ == '__main__':
-    import nibconfig
-    reload(nibconfig)
-    cfg = nibconfig.Configurator(['/Users/scottburns/Documents/code/nibble/configurations/cutting_global.yaml',
-                        '/Users/scottburns/Documents/code/nibble/configurations/LDRC_KKI.yaml'])
-    all_data = cfg.load_yaml()
+    test_config()
