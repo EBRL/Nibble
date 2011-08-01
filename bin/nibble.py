be2cd1ecc3ba65ea71dbd8d4694c0fab3ae9526a
Scott Burns <scott.s.burns@gmail.com>
2011-08-01 13:41:38 -0500

    clean up

--- bin/nibble.py
+++ bin/nibble.py
@@ -5,17 +5,37 @@ Copyright (c) 2011, Scott Burns
 All rights reserved.
 """
 
-def test_config(input_list, output_path):
-    from nibble import config
-    reload(config)
-    cfg = config.Configurator(input_list)
-    all_data = cfg.load_yaml(verbose=False)
-    cfg.save_yaml(output_path)
+from nibble import config
+reload(config)
+from nibble import spm
+reload(spm)
 
 
 if __name__ == '__main__':
     input_config = ['/Users/scottburns/Documents/code/Nibble/configurations/cutting_global.yaml',
                         '/Users/scottburns/Documents/code/Nibble/configurations/LDRC_KKI.yaml']
-    output_path = '/Users/scottburns/Documents/code/Nibble/configurations/LDRC_KK_final.yaml'
-    test_config(input_config, output_path)
+    output_path = '/Users/scottburns/Documents/code/Nibble/configurations/generated/LDRC_KKI_final.yaml'
+
+    #starting with config
+    cfg = config.Configurator(input_config)
+    all_data = cfg.load_yaml(verbose=False)
+    cfg.save_yaml(output_path)
+        
+    print('')
+    print('')
     
+    # starting with SPM
+    total = config.yaml2data(output_path)
+    ver = total['version']
+    if ver == 1:
+        project = total['project']
+        tdl = project['todo']
+        paradigms = project['paradigms']
+        subjects = project['subjects']
+        for stream in tdl: # only spm for now
+            pieces = tdl[stream]
+            for piece in pieces:
+                for paradigm in paradigms:
+                    for subj in subjects:
+                        if stream == 'SPM':
+                           spm_obj = spm.SPM(subj, paradigm, piece, total)
